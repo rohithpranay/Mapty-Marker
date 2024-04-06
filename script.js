00430809bf6f33ce2bae4d22a3a01012a57c871f
rohithpranay <pranayrohith@gmail.com>
2024-04-06 16:40:36 +0530

    Added Leaflet api

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      const { latitude, longitude } = pos.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on("click", function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("hello")
+          .openPopup();
+      });
+    },
+    function () {
+      alert("could not get ur position");
+    }
+  );
